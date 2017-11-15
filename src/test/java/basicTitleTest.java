74a21c748741fd3b7507ebcbf873cf11ce6b4e0d
DTS_AdopwithChef <dts_adopwithchef@ldap.adop.com>
2017-11-15 10:39:07 +0000

    Update basicTitleTest.java

--- src/test/java/basicTitleTest.java
+++ src/test/java/basicTitleTest.java
@@ -29,8 +29,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class basicTitleTest {
 	
 	//URL to visit
-	public String QWE="http://tomcat.10.10.1.201.xip.io/CurrencyConverter-1/";
-	
+	public String QWE="http://10.10.1.201:8080/CurrencyConverter-1/";
+
 	//Title to check
 	public String QWE1="Currency Converter";
 	public String nodeURL = "http://selenium-hub:4444/wd/hub";
